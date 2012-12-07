01317a691809c5393d2dfd10ebbac6c4de7d9c11
TripNRaVeR <noerialexeew@live.nl>
2012-12-07 22:21:01 +0100

    import tripndroid filemanager source

---
+++ src/tripndroid/filemanager/DistributionLibraryListActivity.java
@@ -0,0 +1,16 @@
+package tripndroid.cmfilemanager;
+
+import android.app.Dialog;
+import android.app.ListActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class DistributionLibraryListActivity extends ListActivity {
+
+    /** Called when the activity is first created. */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
